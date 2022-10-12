5fd1a913c905bd48591ea32ec5748e6dcdc7689c
kenneth <kennethkimken@gmail.com>
2022-10-12 22:03:24 +0300

    Updates

---
+++ precache-manifest.2f8a132b16808d604a4cda89cbff0d0c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e25cef100d23c4f474ff",
+    "url": "/generic-company-landing-page/static/css/main.ebb1ea71.chunk.css"
+  },
+  {
+    "revision": "e25cef100d23c4f474ff",
+    "url": "/generic-company-landing-page/static/js/main.8e348f4e.chunk.js"
+  },
+  {
+    "revision": "aba447115b2b9e605cc0",
+    "url": "/generic-company-landing-page/static/js/runtime~main.a5e0cae4.js"
+  },
+  {
+    "revision": "7b79fdd728a0c25d663a",
+    "url": "/generic-company-landing-page/static/css/2.8605ef01.chunk.css"
+  },
+  {
+    "revision": "7b79fdd728a0c25d663a",
+    "url": "/generic-company-landing-page/static/js/2.a07a35bb.chunk.js"
+  },
+  {
+    "revision": "9dc639999e908e298750",
+    "url": "/generic-company-landing-page/static/js/3.1090266c.chunk.js"
+  },
+  {
+    "revision": "996e2983d312598efc2ccf6a5435b5f9",
+    "url": "/generic-company-landing-page/static/media/headerimg.996e2983.png"
+  },
+  {
+    "revision": "aad63b36418a6d9bf67dccbd22c14e84",
+    "url": "/generic-company-landing-page/static/media/contact-us.aad63b36.png"
+  },
+  {
+    "revision": "2ed3be3e2a29e6fbce2b57736093c5a2",
+    "url": "/generic-company-landing-page/static/media/about-us.2ed3be3e.png"
+  },
+  {
+    "revision": "12966908e0e29f65240c94b1b386e5cf",
+    "url": "/generic-company-landing-page/index.html"
+  }
+];
